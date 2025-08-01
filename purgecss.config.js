d77ac0ef4fffe1639eea76fd495a6ed99fcb610a
Nikorasu47 <normal47@live.fr>
2025-08-01 16:39:14 +0200

    purge css

---
+++ purgecss.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  content: ["index.html", "./assets/*.js"],
+  css: ["./assets/style.css", "./assets/bootstrap/*.css"],
+  keyframs: true,
+  output: "./assets/clean-bootstrap/",
+  rejected: true,
+  variables: true,
+};
