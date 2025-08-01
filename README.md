77dffe87adf1a7f6594a2d596d7dc0dcd7bab85c
Nikorasu47 <normal47@live.fr>
2025-08-01 15:51:02 +0200

    js actu

--- README.md
+++
@@ -1,2 +0,0 @@
-# NinaOptiSite
-premiere optimisation seo
--- assets/scripts.js
+++ assets/scripts.js
@@ -1,15 +1,6 @@
-$(document).ready(function() {
-    $('.gallery').mauGallery({
-        columns: {
-            xs: 1,
-            sm: 2,
-            md: 3,
-            lg: 3,
-            xl: 3
-        },
-        lightBox: true,
-        lightboxId: 'myAwesomeLightbox',
-        showTags: true,
-        tagsPosition: 'top'
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  $('.gallery').mauGallery({
+    columns: 3,
+    lightBox: true
+  });
 });
